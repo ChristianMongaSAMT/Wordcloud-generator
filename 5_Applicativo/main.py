4e8b4e346b7f7021c883ec7c9222a49e9fec37a2
AlessandroCuriale <alessandro.curiale@CPT.local>
2023-02-03 16:04:48 +0100

    03.02.2023 Final

--- 5_Applicativo/main.py
+++ 5_Applicativo/main.py
@@ -7,30 +7,66 @@ from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
 from kivy.core.window import Window
+from pathlib import Path
+from kivy.config import Config
 
 
 logmanager.get_configured_logger()
-# __drag and drop__ 
-class DragAndDrop():
+Config.read('./config.ini')
+
+class TestRead():   
+
+    def __init__(self, path):
+        logging.debug(path.__class__)
+
+        self.p = Path(path)
+
+    def printData(self):
+        logging.debug(f'{self.p.read_text()}')
+
+########################DragNDrop############################
+'''class DragAndDrop():
     def build(self):
         Window.bind(on_drop_file=self._on_file_drop)
-        return Label(text = "Drag and Drop File here")
-
-    def _on_file_drop(self, window, file_path,x ,y):
-        fp = open(file_path, 'r')
-        print(fp.read())
+        #self.ids['l1'].bind(on_drop_file=self._on_file_drop)
         return
 
+    def _on_file_drop(self, window, file_path, x, y):
+        #logging.debug(f'file: {file_path}, drop location ({x};{y})')
+        #test = TestRead(file_path.decode("utf-8") )
+        print(f'drop {file_path.decode("utf-8")} location ({x},{y})')
+        #Logger.info(f'drop {file_path.decode("utf-8")} location ({x},{y})')
+        #test.printData()
+        return
+'''
 class WordCloudGUI(BoxLayout):
-    pass
+    pTemp = "PERCORSO DA INSERIRE"
+    #def __init__(self):
+        #self.ids['l1'].text
+    def build(self):
+        self.ids['l1'].bind(on_drop_file=self._on_file_drop)
+
+    def _on_file_drop(self, window, file_path, x, y):
+        #logging.debug(f'file: {file_path}, drop location ({x};{y})')
+        #test = TestRead(file_path.decode("utf-8") )
+        print(f'drop {file_path.decode("utf-8")} location ({x},{y})')
+        #Logger.info(f'drop {file_path.decode("utf-8")} location ({x},{y})')
+        #test.printData()
+        return   
 
 class WordCloudApp(App):
     def build(self):
         return WordCloudGUI()
+
+
+     
+        
 
 # def run():
     
 
 if __name__ == '__main__':
     WordCloudApp().run()
+    
