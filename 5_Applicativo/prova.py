e0245d4ec06960d05fc58b0cbffa432cd89f29e1
edoardoratti <edoardo.ratti@CPT.local>
2023-02-03 14:18:56 +0100

    DragNDrop + stampa path

--- 5_Applicativo/prova.py
+++ 5_Applicativo/prova.py
@@ -13,58 +13,33 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 from kivy.core.window import Window
 
-logmanager.get_configured_logger()
 
-class Test():   
-    folder = ""
+
+class TestRead():   
 
     def __init__(self, path):
-        self.folder = Path(path)
+        logging.debug(path.__class__)
+        print(f'path class: {path.__class__}')
+
+        self.p = Path(path)
 
     def printData(self):
-        logging.debug(f'{self.folder.read_text()}')
-        
-class DragLabel(DragBehavior, Label):
-    pass
-########################DragBehavior############################
-class TestApp(App):
+        logging.debug(f'{self.p.read_text()}')
+        print(f'file content: {self.p.read_text()}')
 
-    def build(self):
-        self.title = 'ztest'
-########################TestPath############################
-#def run():
-#    test = Test('E:\\306\\Worldcloud-generatorData\\testingPath.txt')
-#    test.printData()
-
-########################DragNDrop############################
-"""class DraggableBoxLayout(DraggableLayoutBehavior, BoxLayout):
-
-    def compare_pos_to_widget(self, widget, pos):
-        if self.orientation == 'vertical':
-            return 'before' if pos[1] >= widget.center_y else 'after'
-        return 'before' if pos[0] < widget.center_x else 'after'
-
-    def handle_drag_release(self, index, drag_widget):
-        self.add_widget(drag_widget, index)
-
-class DragLabel(DraggableObjectBehavior, Label):
-
-    def initiate_drag(self):
-        # during a drag, we remove the widget from the original location
-        self.parent.remove_widget(self)"""
 ########################DragNDrop############################
 class WindowFileDropExampleApp(App):
     def build(self):
-        Window.bind(on_dropfile=self._on_file_drop)
-        
-        #Window.bind(on_drop_file=self.on_file_drop)
+        Window.bind(on_drop_file=self._on_file_drop)
         return
 
-    def _on_file_drop(self, window, file_path):
-        print(file_path)
+    def _on_file_drop(self, window, file_path, x, y):
+        logging.debug(f'file: {file_path}, drop location ({x};{y})')
+        test = TestRead(file_path.decode("utf-8") )
+        test.printData()
         return
 
 ####################################################################
 if __name__ == '__main__':
-    #TestApp().run()
+    logmanager.get_configured_logger()
     WindowFileDropExampleApp().run()
