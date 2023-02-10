2e324492bba68785db765b4d47435e732930b3f5
AlessandroCuriale <alessandro.curiale@CPT.local>
2023-02-10 09:35:01 +0100

    imageViewer

---
+++ 5_Applicativo/curialetest.py
@@ -0,0 +1,36 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from pathlib import Path
+from kivy.config import Config
+
+
+Config.read('./config.ini')
+
+
+
+class CurialeGUI(BoxLayout): 
+    pass
+
+class CurialeApp(App):
+    path = "./pictures/class.png"
+
+    def build(self):
+        return CurialeGUI()
+
+    def process(self):
+        self.path = self.root.ids.path.text
+        print(self.path)
+
+    def visualizer(self):
+        self.root.ids.image.source = self.path
+
+if __name__ == '__main__':
+    CurialeApp().run()
+    
