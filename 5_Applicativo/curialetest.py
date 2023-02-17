d956ce4f9e4f925c0bbf9bf38664387da8f9f367
AlessandroCuriale <alessandro.curiale@CPT.local>
2023-02-17 11:33:28 +0100

    list words

--- 5_Applicativo/curialetest.py
+++ 5_Applicativo/curialetest.py
@@ -9,11 +9,11 @@ from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 from pathlib import Path
 from kivy.config import Config
-import re
+
 import os
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-import requests
+import validators
 
 Config.read('./config.ini')
 
@@ -25,27 +25,37 @@ class CurialeApp(App):
     words = ""
     excludedWords = open("./text/excludedWords.txt", "r").read().rsplit(",")
     isWordValid = True
+    proxySetup = []
+    isProxySetup = False
 
-
+    def build(self):
+        return CurialeGUI()
     
-    def remove_tags(html):
+    def remove_tags(self, html):
         soup = BeautifulSoup(html, "html.parser")
         for data in soup(['style', 'script']):
             data.decompose()
         return ' '.join(soup.stripped_strings)
- 
-    
-    link = "https://stackoverflow.com/questions/15138614/how-can-i-read-the-contents-of-an-url-with-python"
-    #proxy_env = os.environ['JAVA_HOME']
-    #print("ENV:", proxy_env)
-    for name, value in os.environ.items():
-        print("{0}: {1}".format(name, value))
-    #f = urlopen(link)
-    #myfile = f.read()
-    #print(remove_tags(myfile))
 
-    def build(self):
-        return CurialeGUI()
+    def setProxyIfExist(self):
+        if(not(self.isProxySetup)):
+            for name, value in os.environ.items():
+                if(name.upper() == "HTTP_PROXY" or name.upper() == "HTTPS_PROXY"):
+                    self.proxySetup.append(f"{name}: {value}")
+                    print(f"{self.proxySetup}")
+            self.isProxySetup = True
+
+
+    def getWordsFromUri(self):
+        self.setProxyIfExist()
+        link = self.root.ids.pathUri.text
+        if(validators.url(link)):
+            f = urlopen(link)
+            myfile = f.read()
+            self.words = str(self.remove_tags(myfile))
+        
+
+    #fflink = "https://stackoverflow.com/questions/15138614/how-can-i-read-the-contents-of-an-url-with-python"
 
     def getWords(self):
         self.wordFile = self.root.ids.pathWords.text
@@ -53,29 +63,32 @@ class CurialeApp(App):
         if(os.path.isfile(self.wordFile)):
             self.words = open(self.wordFile, "r").read()
             
-
-    def generateListWord(self, isFile):
-        if(isFile):
+    def generateListWord(self, type):
+        if(type == "FILE"):
             self.getWords()
+        elif(type == "URL"):
+            self.getWordsFromUri()
         else:
             self.words =  self.root.ids.wordsList.text
         for character in self.words:
+            
             #isalpha accetta anche i caratteri speiali come "?", "!", "@"
             #isalpha  or  (character >= 'a' and character <= 'z' or character >= 'A' and character <= 'Z')
             if(not(character >= 'a' and character <= 'z' or character >= 'A' and character <= 'Z')):
                 self.words = self.words.replace(character, " ")
         if(len(self.words) > 0):
-            print = ""
+            printer = "ciao"
             self.words = self.words.rsplit(" ")
             for word in self.words:
+                print(word)
                 if(word != ""):
                     for excludedWord in self.excludedWords:
                         if(word.lower() == excludedWord.lower()):
                             self.isWordValid = False
                     if(self.isWordValid):
-                        print += word + "\n"
+                        printer += word + "\n"
                 self.isWordValid = True
-            self.root.ids.result.text = print
+            self.root.ids.result.text = printer
 
 
     def process(self):
