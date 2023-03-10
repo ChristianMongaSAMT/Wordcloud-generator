5c4761804f615a46e87b52c02ac70f5235e8f0d2
Edoardo Ratti <edoardo.ratti@CPT.local>
2023-03-10 16:28:43 +0100

    Algoritmo selezione

--- 5_Applicativo/testCont.py
+++ 5_Applicativo/testCont.py
@@ -36,6 +36,7 @@ class ConterGUI(BoxLayout):
 
         screenDim = self.ids.image.size
         self.imageDim = self.ids.image.norm_image_size
+        self.popList = []
 
         
 
@@ -48,16 +49,14 @@ class ConterGUI(BoxLayout):
         
         print("pos", touch.pos)
         print("img", self.imageDim)
-        print("dim", screenDim, "\n")
+        print("dim", screenDim)
         
         pxY = int(y-(screenDim[1] - self.imageDim[1])/2)
         pxX = int(x-(screenDim[0] - self.imageDim[0])/2)
-        print("X --> ", pxX)
-        #print(BORDI)
         if(self.isInArea(pxX,pxY)):
             
-            print("cpos",pxX, pxY, "????????????????????")
-            img[pxX,pxY] = [255,255,0]
+            print("rpo",pxX, pxY, "\n")
+            #img[pxX,pxY] = [255,255,0]
             x = pxX - 5
             y = pxY - 5
             """while(x < pxX + 5):
@@ -76,34 +75,56 @@ class ConterGUI(BoxLayout):
             self.ids.image.reload()
 
     def highlightArea(self, img, x, y):
+        img[y, x] = [0,255,0]
         if(self.isInArea(x, y)):
-            popList = []
             img[y, x] = [0,255,0]
             if(self.isInArea(x + 1,y)):
-                pixel = [x + 1, y, False]
-                popList.append(pixel)
+                pixel = {'x':x + 1, 'y':y, 'check':False}
+                self.popList.append(pixel)
             if(self.isInArea(x - 1,y)):
-                pixel = [x - 1, y, False]
-                popList.append(pixel)
+                pixel ={'x':x - 1, 'y':y, 'check':False}
+                self.popList.append(pixel)
             if(self.isInArea(x,y + 1)):
-                pixel = [x, y + 1, False]
-                popList.append(pixel)
+                pixel = {'x':x, 'y':y+1, 'check':False}
+                self.popList.append(pixel)
             if(self.isInArea(x,y - 1)):
-                pixel = [x, y - 1, False]
-                popList.append(pixel) 
-            while(False in popList):
-                if(self.isInArea(x + 1,y)):
-                    pixel = [x + 1, y, False]
-                    popList.append(pixel)
-                if(self.isInArea(x - 1,y)):
-                    pixel = [x - 1, y, False]
-                    popList.append(pixel)
-                if(self.isInArea(x,y + 1)):
-                    pixel = [x, y + 1, False]
-                    popList.append(pixel)
-                if(self.isInArea(x,y - 1)):
-                    pixel = [x, y - 1, False]
-                    popList.append(pixel) 
+                pixel = {'x':x , 'y':y-1, 'check':False}
+                self.popList.append(pixel)
+            
+            #print(self.popList)
+            #print("Salca",self.arrayFirstIndexOf(False)[1])
+            #(element, index) = self.arrayFirstIndexOf(False)
+            index = 0
+            element = self.popList[index]
+            cicle = 10000
+            c = 0
+            while element and not element['check'] and c < cicle:
+                self.popList[index]['check'] = True
+                x = element['x']
+                y = element['y']
+                img[y, x] = [0,255,0]
+                c+=1        
+                
+                if(self.isInArea(x + 1,y) and not self.isHighlightedPixel(x + 1, y, img)):
+                    pixel = {'x':x + 1, 'y':y, 'check':False}
+                    print(f'adding new pixel RIGHT: {pixel}')
+                    self.popList.append(pixel)
+                if(self.isInArea(x - 1,y) and not self.isHighlightedPixel(x - 1, y, img)):
+                    pixel ={'x':x - 1, 'y':y, 'check':False}
+                    print(f'adding new pixel LEFT: {pixel}')
+                    self.popList.append(pixel)
+                if(self.isInArea(x,y + 1) and not self.isHighlightedPixel(x, y + 1, img)):
+                    pixel = {'x':x, 'y':y+1, 'check':False}
+                    print(f'adding new pixel UP: {pixel}')
+                    self.popList.append(pixel)
+                if(self.isInArea(x,y - 1) and not self.isHighlightedPixel(x, y - 1, img)):
+                    pixel = {'x':x , 'y':y-1, 'check':False}
+                    print(f'adding new pixel DOWN: {pixel}')
+                    self.popList.append(pixel)
+                #loading next element
+                #(element, index) = self.arrayFirstIndexOf(False)   
+                index += 1  
+                element = self.popList[index]
 
         """if(self.isInArea(x,y) and  not self.isHighlightedPixel(x,y, img)):
             img[y, x] = [0,255,0]
@@ -125,6 +146,13 @@ class ConterGUI(BoxLayout):
             if(self.isInBorder(x, y - 1)):
                 self.highlightArea(img, x, y - 1, imageDim)"""
 
+    def arrayFirstIndexOf(self, val):
+        for index, element in enumerate(self.popList):
+            if (element['check'] == val):
+                print(f'first element with value {val}: {element}, {index}')
+                return element, index
+        return None, None
+
     def popNewPixels(self, x, y):
         if(self.isInArea(x + 1,y)):
             pixel = [x + 1, y]
@@ -142,6 +170,7 @@ class ConterGUI(BoxLayout):
             pixel = [x, y - 1]
             popList.append(pixel) 
             popCheckList.append(False)
+
     def isHighlightedPixel(self, x, y, img):
         test = np.array([0,255,0])
         isEqual = True
@@ -165,9 +194,9 @@ class ConterGUI(BoxLayout):
         #print(x < imageDim[0], "x < 500 | x = ", x)
         #print(y < imageDim[1], "y < 500", "\n")
         if(x > 0 and y > 0 and x < self.imageDim[0] and y < self.imageDim[1]):
-            print(f'x: {x}, y: {y}, w: {self.imageDim[0]}, h: {self.imageDim[1]}, in: True')
+            #print(f'x: {x}, y: {y}, w: {self.imageDim[0]}, h: {self.imageDim[1]}, in: True')
             return True
-        print(f'x: {x}, y: {y}, w: {self.imageDim[0]}, h: {self.imageDim[1]}, in: False')
+        #print(f'x: {x}, y: {y}, w: {self.imageDim[0]}, h: {self.imageDim[1]}, in: False')
         return False
         
 class ConterApp(App):
