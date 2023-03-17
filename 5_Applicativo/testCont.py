c5fb41c75109a491a7e4b0beb427ebf96e579bea
Edoardo Ratti <edoardo.ratti@CPT.local>
2023-03-17 15:07:48 +0100

    Algoritmo Flood Fill completato

--- 5_Applicativo/testCont.py
+++ 5_Applicativo/testCont.py
@@ -46,7 +46,7 @@ class ConterGUI(BoxLayout):
         else:
             d = screenDim[1]/2 - y
             y = screenDim[1]/2 + d
-        
+        print(BORDI)
         print("pos", touch.pos)
         print("img", self.imageDim)
         print("dim", screenDim)
@@ -66,110 +66,59 @@ class ConterGUI(BoxLayout):
                     y+=1                
                 x += 1
                 y = pxY - 5"""
-            print("Pura curiosità ", BORDI[4][1][0][1]) #[bordo][pixel][0--> evitare il valore strano][coordinata]
-            #print("atisoiruc aruP ", "\n",GERARCHIA)
+           
+            #print("Pura curiosità ", BORDI[4][1][0][1]) #[bordo][pixel][0--> evitare il valore strano][coordinata]
             self.highlightArea(img, pxX, pxY)
+            print(self.isBorder(pxX, pxY, img))
             #path = "./pictures/provaEdo.png"
             cv2.imwrite(path, img)
             self.ids.image.source = path
             self.ids.image.reload()
 
     def highlightArea(self, img, x, y):
-        img[y, x] = [0,255,0]
-        if(self.isInArea(x, y)):
-            img[y, x] = [0,255,0]
-            if(self.isInArea(x + 1,y)):
-                pixel = {'x':x + 1, 'y':y, 'check':False}
-                self.popList.append(pixel)
-            if(self.isInArea(x - 1,y)):
-                pixel ={'x':x - 1, 'y':y, 'check':False}
-                self.popList.append(pixel)
-            if(self.isInArea(x,y + 1)):
-                pixel = {'x':x, 'y':y+1, 'check':False}
-                self.popList.append(pixel)
-            if(self.isInArea(x,y - 1)):
-                pixel = {'x':x , 'y':y-1, 'check':False}
-                self.popList.append(pixel)
+        queue = []
+        # Append the position of starting
+        # pixel of the component
+        queue.append([x, y])
+        # Color the pixel with the new color
+        color = [0,255,0]
+        img[y][x] = color
+        
+        # While the queue is not empty i.e. the
+        # whole component having prevC color
+        # is not colored with newC color
+        while queue:
             
-            #print(self.popList)
-            #print("Salca",self.arrayFirstIndexOf(False)[1])
-            #(element, index) = self.arrayFirstIndexOf(False)
-            index = 0
-            element = self.popList[index]
-            cicle = 10000
-            c = 0
-            while element and not element['check'] and c < cicle:
-                self.popList[index]['check'] = True
-                x = element['x']
-                y = element['y']
-                img[y, x] = [0,255,0]
-                c+=1        
-                
-                if(self.isInArea(x + 1,y) and not self.isHighlightedPixel(x + 1, y, img)):
-                    pixel = {'x':x + 1, 'y':y, 'check':False}
-                    print(f'adding new pixel RIGHT: {pixel}')
-                    self.popList.append(pixel)
-                if(self.isInArea(x - 1,y) and not self.isHighlightedPixel(x - 1, y, img)):
-                    pixel ={'x':x - 1, 'y':y, 'check':False}
-                    print(f'adding new pixel LEFT: {pixel}')
-                    self.popList.append(pixel)
-                if(self.isInArea(x,y + 1) and not self.isHighlightedPixel(x, y + 1, img)):
-                    pixel = {'x':x, 'y':y+1, 'check':False}
-                    print(f'adding new pixel UP: {pixel}')
-                    self.popList.append(pixel)
-                if(self.isInArea(x,y - 1) and not self.isHighlightedPixel(x, y - 1, img)):
-                    pixel = {'x':x , 'y':y-1, 'check':False}
-                    print(f'adding new pixel DOWN: {pixel}')
-                    self.popList.append(pixel)
-                #loading next element
-                #(element, index) = self.arrayFirstIndexOf(False)   
-                index += 1  
-                element = self.popList[index]
+            # Dequeue the front node
+            currPixel = queue.pop()
+            
+            posX = currPixel[0]
+            posY = currPixel[1]
+            
+            # Check if the adjacent
+            # pixels are valid
+            if(self.isInArea(posX + 1, posY) and not self.isHighlightedPixel(posX + 1, posY, img) and not self.isBorder(posX + 1, posY, img)):
+                img[posY][posX + 1] = color
+                queue.append([posX + 1, posY])
 
-        """if(self.isInArea(x,y) and  not self.isHighlightedPixel(x,y, img)):
-            img[y, x] = [0,255,0]
-            if(self.isInArea(x + 1,y)):
-                self.highlightArea(img, x + 1, y)
-            if(self.isInArea(x - 1,y)):
-                self.highlightArea(img, x - 1, y)
-            if(self.isInArea(x,y + 1)):
-                self.highlightArea(img, x, y + 1)
-            if(self.isInArea(x,y - 1)):
-                self.highlightArea(img, x, y - 1)
-
-            if(self.isInBorder(x + 1, y)):
-                self.highlightArea(img, x + 1, y, imageDim)
-            if(self.isInBorder(x - 1, y)):
-                self.highlightArea(img, x - 1, y, imageDim)
-            if(self.isInBorder(x, y + 1)):
-                self.highlightArea(img, x, y + 1, imageDim)
-            if(self.isInBorder(x, y - 1)):
-                self.highlightArea(img, x, y - 1, imageDim)"""
-
-    def arrayFirstIndexOf(self, val):
+            if(self.isInArea(posX - 1, posY) and not self.isHighlightedPixel(posX - 1, posY, img) and not self.isBorder(posX - 1, posY, img)):
+                img[posY][posX - 1] = color
+                queue.append([posX - 1, posY])
+            
+            if(self.isInArea(posX, posY + 1) and not self.isHighlightedPixel(posX, posY + 1, img) and not self.isBorder(posX, posY + 1, img)):
+                img[posY + 1][posX] = color
+                queue.append([posX, posY + 1])
+            
+            if( self.isInArea(posX, posY + 1) and not self.isHighlightedPixel(posX, posY - 1, img) and not self.isBorder(posX, posY - 1, img)):
+                img[posY - 1][posX] = color
+                queue.append([posX, posY - 1])
+ 
+    """def arrayFirstIndexOf(self, val):
         for index, element in enumerate(self.popList):
             if (element['check'] == val):
                 print(f'first element with value {val}: {element}, {index}')
                 return element, index
-        return None, None
-
-    def popNewPixels(self, x, y):
-        if(self.isInArea(x + 1,y)):
-            pixel = [x + 1, y]
-            popList.append(pixel)
-            popCheckList.append(False)
-        if(self.isInArea(x - 1,y)):
-            pixel = [x - 1, y]
-            popList.append(pixel)
-            popCheckList.append(False)
-        if(self.isInArea(x,y + 1)):
-            pixel = [x, y + 1]
-            popList.append(pixel)
-            popCheckList.append(False)
-        if(self.isInArea(x,y - 1)):
-            pixel = [x, y - 1]
-            popList.append(pixel) 
-            popCheckList.append(False)
+        return None, None"""
 
     def isHighlightedPixel(self, x, y, img):
         test = np.array([0,255,0])
@@ -178,29 +127,45 @@ class ConterGUI(BoxLayout):
             if(isEqual):
                 if(not (bgr == test[key])):
                     isEqual = False
+
         return isEqual
 
-    def isInBorder(self, x, y):
+    def isBorder(self, x, y, img):
+        test = np.array([0, 0, 150])
+        isEqual = True
+        for key, bgr in enumerate(img[y,x]):
+            if(isEqual):
+                if(not (bgr == test[key])):
+                    isEqual = False
+
+        return isEqual
         #[bordo][pixel][0--> evitare il valore strano][coordinata]
+        color = img[y][x]
+        bColor = [0, 0,255]
+        if(color == bColor):
+            return True
+        """inABorder = True
+
         for bordo in BORDI:
             for pixel in bordo:
-                if(pixel[0][0] == x  and pixel[0][1] == y):
-                    return False
-        return True
+                print("1")
+                if(pixel[0][0] == x and pixel[0][1] == y):
+                    inABorder = False
+        return inABorder"""
 
     def isInArea(self, x, y):
         #print(x > 0, "x > 0")
         #print(y > 0, "y > 0")
         #print(x < imageDim[0], "x < 500 | x = ", x)
         #print(y < imageDim[1], "y < 500", "\n")
-        if(x > 0 and y > 0 and x < self.imageDim[0] and y < self.imageDim[1]):
+        if(x >= 0 and y >= 0 and x < self.imageDim[0] and y < self.imageDim[1]):
             #print(f'x: {x}, y: {y}, w: {self.imageDim[0]}, h: {self.imageDim[1]}, in: True')
             return True
         #print(f'x: {x}, y: {y}, w: {self.imageDim[0]}, h: {self.imageDim[1]}, in: False')
         return False
         
 class ConterApp(App):
-    path = './pictures/cerchio.png'
+    path = './pictures/heart.png'
     def build(self):            
         self.build_image()
         self.gui = ConterGUI()
@@ -227,7 +192,7 @@ class ConterApp(App):
             if(area > 1000):
                 #print('Area:', area)
                 #print('Perimeter:', perimeter)
-                bordo = cv2.drawContours(img, [cnt], -1, (0,0,255), 3)
+                bordo = cv2.drawContours(img, [cnt], -1, (0,0,150), 1)
                 #self.gui.setBorder("ok")
                 x1, y1 = cnt[0,0]
 
@@ -238,9 +203,6 @@ class ConterApp(App):
         cv2.imwrite(pathTempImage, img)
         #self.root.ids.image.source = pathTempImage
         #self.root.ids.image.reload()
-
-    #def on_touch_down(self, touch):
-    #    print("Mouse Down", touch)
     
     
 
