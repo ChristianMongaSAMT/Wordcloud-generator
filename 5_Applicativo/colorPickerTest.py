58dee0d1918e3dbbee471e6dfad6798bd377670f
Edoardo Ratti <edoardo.ratti@CPT.local>
2023-03-31 15:06:44 +0200

    Scelta formato di output + pulizia generale + Diario

--- 5_Applicativo/colorPickerTest.py
+++ 5_Applicativo/colorPickerTest.py
@@ -25,7 +25,11 @@ import os.path
 import math
 
 BORDER_COLOR = (100,100,100)
+FLOAD_COLOR = (0,0,0)
 DOWNLOAD = -1
+START_PATH = "./pictures/cerchio.png"
+MASK_PATH = "./pictures/provaEdoBN.png"
+TEMP_PATH = "./pictures/provaEdo.png"
 
 Builder.load_string('''
 <MongaGUI>:
@@ -60,13 +64,34 @@ Builder.load_string('''
             id: download_button
             on_release: app.download()        
         Spinner:
-            size_hint: None, None
             size: 100, 44
             text: 'png'
             values:'png','jpg','webp'
             id: formatSpinner
-            on_text:
-                app.format_changed()           
+        TextInput:
+            hint_text: 'Insert width'
+            id: w
+            text_size: self.size
+            size_hint: 1, 0.15
+            on_focus: app.validate_new_dim("w")
+        TextInput:
+            hint_text: 'Insert height'
+            id: h
+            text_size: self.size
+            size_hint: 1, 0.15
+            on_focus: app.validate_new_dim("h")
+        #Slider:
+        #    size: 100, 44
+        #    orientation:'vertical'
+        #    id: w
+        #    min:1
+        #    on_value: app.validate_new_dim()
+        #Slider:
+        #    size: 100, 44
+        #    orientation:'vertical'
+        #    min: 1
+        #    id: h
+        #    on_value: app.validate_new_dim()
 
 ''')
 
@@ -85,10 +110,12 @@ class ImageBox(BoxLayout):
 
     def on_color(self, instance, value):
         global BORDER_COLOR
+        global FLOAD_COLOR
         #print("RGBA = ", str(value))  #  or instance.color
         #print("HSV = ", str(instance.hsv))
         #print("HEX = ", str(instance.hex_color))
         BORDER_COLOR = (int(value[2] * 255), int(value[1] * 255), int(value[0] * 255))
+        FLOAD_COLOR = (255 - BORDER_COLOR[0],255 -BORDER_COLOR[1],255 - BORDER_COLOR[2])
 
 class Download(BoxLayout):
      def __init__(self, **kwargs):
@@ -104,17 +131,15 @@ class ImageSelection(BoxLayout):
         self.buildImage()
     
     def buildImage(self):
-        img = cv2.imread(self.startPath)
-        print(img.shape,"albatros")
+        img = cv2.imread(START_PATH)
         mask = np.zeros((img.shape[0],img.shape[1],1), np.uint8)
 
-        cv2.imwrite("./pictures/provaEdoBN.png", mask, [cv2.IMWRITE_PNG_BILEVEL, 1])
+        cv2.imwrite(MASK_PATH, mask, [cv2.IMWRITE_PNG_BILEVEL, 1])
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         ret,thresh = cv2.threshold(gray,150,255,0)
         #countour[4][1][0][1]) #[bordo][pixel][0--> evitare il valore strano][coordinata]
         self.contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        #BORDERS = contours
         for cnt in self.contours:
             area = cv2.contourArea(cnt)
             perimeter = cv2.arcLength(cnt, True)
@@ -123,13 +148,11 @@ class ImageSelection(BoxLayout):
             if(area > 1000):
                 cv2.drawContours(img, [cnt], -1, BORDER_COLOR, 1)
                 x1, y1 = cnt[0,0]
-        pathTempImage = "./pictures/provaEdo.png"
-        cv2.imwrite(pathTempImage, img)
+        cv2.imwrite(TEMP_PATH, img)
     
     def on_touch_down(self, touch):
-        path = './pictures/provaEdo.png'
-        img = cv2.imread(path)
-        mask = cv2.imread('./pictures/provaEdoBN.png', cv2.IMREAD_UNCHANGED)
+        img = cv2.imread(TEMP_PATH)
+        mask = cv2.imread(MASK_PATH, cv2.IMREAD_UNCHANGED)
         for cnt in self.contours:
             area = cv2.contourArea(cnt)
             perimeter = cv2.arcLength(cnt, True)
@@ -138,10 +161,9 @@ class ImageSelection(BoxLayout):
             if(area > 1000):
                 cv2.drawContours(img, [cnt], -1, BORDER_COLOR, 1)
                 x1, y1 = cnt[0,0]
-        x = math.trunc(touch.pos[0])
-        
-        y = math.trunc(touch.pos[1])
 
+        x = math.trunc(touch.pos[0])
+        y = math.trunc(touch.pos[1])
         screenDim = self.ids.image.size
         self.imageDim = self.ids.image.norm_image_size
 
@@ -159,21 +181,11 @@ class ImageSelection(BoxLayout):
         pxY = int(y-(screenDim[1] - self.imageDim[1])/2)
         pxX = int(x-(screenDim[0] - self.imageDim[0])/2)
         if(self.isInArea(pxX,pxY)):
-            
             print("rpo",pxX, pxY, "\n")
-            #img[pxX,pxY] = [255,255,0]
-            x = pxX - 5
-            y = pxY - 5
-            """while(x < pxX + 5):
-                while(y < pxY + 5):
-                    if(self.isInArea(y,x, imageDim)):
-                        img[y, x] = [255,255,0]
-                    y+=1                
-                x += 1
-                y = pxY - 5"""
             self.highlightArea(img, mask, pxX, pxY)
-            cv2.imwrite('./pictures/provaEdoBN.png', mask, [cv2.IMWRITE_PNG_BILEVEL, 1])
-            self.ids.image.source = path
+            cv2.imwrite(MASK_PATH, mask, [cv2.IMWRITE_PNG_BILEVEL, 1])
+            cv2.imwrite(TEMP_PATH, img)
+            self.ids.image.source = TEMP_PATH 
             self.ids.image.reload()
 
     def highlightArea(self, img, mask, x, y):
@@ -182,42 +194,37 @@ class ImageSelection(BoxLayout):
         # Color the pixel with the new color
         #color = [0,255,0]
         color = 1
-        #color = BORDER_COLOR
-
         mask[y][x] = color
+        img[y][x] = FLOAD_COLOR
 
         while queue:
-            
             currPixel = queue.pop()
             
             posX = currPixel[0]
             posY = currPixel[1]
             if(self.isInArea(posX + 1, posY) and not self.isHighlightedPixel(posX + 1, posY, mask) and not self.isBorder(posX + 1, posY, img)):
                 mask[posY][posX + 1] = color
+                img[posY][posX + 1] = FLOAD_COLOR
                 queue.append([posX + 1, posY])
 
             if(self.isInArea(posX - 1, posY) and not self.isHighlightedPixel(posX - 1, posY, mask) and not self.isBorder(posX - 1, posY, img)):
                 mask[posY][posX - 1] = color
+                img[posY][posX - 1] = FLOAD_COLOR
                 queue.append([posX - 1, posY])
             
             if(self.isInArea(posX, posY + 1) and not self.isHighlightedPixel(posX, posY + 1, mask) and not self.isBorder(posX, posY + 1, img)):
                 mask[posY + 1][posX] = color
+                img[posY + 1][posX] = FLOAD_COLOR
                 queue.append([posX, posY + 1])
             
             if( self.isInArea(posX, posY + 1) and not self.isHighlightedPixel(posX, posY - 1, mask) and not self.isBorder(posX, posY - 1, img)):
                 mask[posY - 1][posX] = color
+                img[posY - 1][posX] = FLOAD_COLOR
                 queue.append([posX, posY - 1])
 
     def isHighlightedPixel(self, x, y, img):
         color = 1
         return color == img[y,x]
-        """isEqual = True
-        for key, bgr in enumerate(img[y,x]):
-            if(isEqual):
-                if(not (bgr == color[key])):
-                    isEqual = False"""
-
-        return isEqual
 
     def isBorder(self, x, y, img):
         isEqual = True
@@ -238,22 +245,43 @@ class MongaGUI(BoxLayout):
     pass
 
 class MongaApp(App):
+    dimensions = ()
     def download(self):
         font = DOWNLOAD.ids.formatSpinner.text
         homedir = os.path.expanduser("~")
         homedir = homedir.replace("\\", "/")
         path = f"{homedir}/Downloads/bellagianda.{font}"# da qui ho capitò che non usero mai piu python
 
-        image = Image.open("./pictures/provaEdoBN.png")
+        image = Image.open(TEMP_PATH)
+        if(self.dimensions.count == 2):
+            image = image.resize(self.dimensions)
+        else:
+            image = image.resize((image.width, image.height))
+        print(self.dimensions)
         image.save(path)
-       
-        print(homedir)
 
-    def format_changed(self):
+
+    def validate_new_dim(self, dimension):
         global DOWNLOAD
-        font = DOWNLOAD.ids.formatSpinner.text
-        #print(font)
-        #self.root.ids.fontLabel.font_name = font
+        img = cv2.imread(START_PATH)
+        divider = 1
+        widthBase = img.shape[0]
+        heightBase = img.shape[1]
+        try:
+            if(dimension == "w"):
+                width = int(DOWNLOAD.ids.w.text)
+                divider = widthBase/heightBase
+                self.dimensions = (width, round(width * divider))
+                DOWNLOAD.ids.h.text = str(round(self.dimensions[1]))
+            elif(dimension == "h"):
+                height = int(DOWNLOAD.ids.h.text)
+                divider = heightBase/widthBase
+                self.dimensions = (round(height * divider), height)
+                DOWNLOAD.ids.w.text = str(round(self.dimensions[0]))
+        except ValueError:
+            print("some values isn't number!")
+        print(dimension)
+        #print(f"h:{height} w:{width}\n")
 
     def build(self):
         mongaGUI = MongaGUI()
